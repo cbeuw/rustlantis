c5c965d5f18d21c4005e377a8d3cebc4eca576c2
Qian Wang <wangqian@student.ethz.ch>
2023-02-20 16:42:25 +0100

    Fix lifetimes

--- fuzz/src/place.rs
+++ fuzz/src/place.rs
@@ -5,45 +5,53 @@ use crate::generation::GenerationCtx;
 
 pub struct PlaceSelector<'ctx> {
     ctx: &'ctx GenerationCtx,
-    candidates: Box<dyn Iterator<Item = (Local, &'ctx LocalDecl)>>,
+    candidates: Box<dyn Iterator<Item = Local> + 'ctx>,
 }
 
 impl<'ctx> PlaceSelector<'ctx> {
     pub fn locals_and_args(ctx: &'ctx GenerationCtx) -> Self {
         Self {
             ctx,
-            candidates: Box::new(ctx.current_body.vars_and_args_decl_iter()),
+            candidates: Box::new(ctx.current_body.vars_and_args_iter()),
         }
     }
     pub fn locals(ctx: &'ctx GenerationCtx) -> Self {
         Self {
             ctx,
-            candidates: Box::new(ctx.current_body.vars_decl_iter()),
+            candidates: Box::new(ctx.current_body.vars_iter()),
         }
     }
 
     pub fn mutable(self) -> Self {
-        self.candidates = Box::new(
-            self.candidates
-                .filter(|(local, decl)| decl.mutability == Mutability::Mut),
-        );
-        self
+        let candidates = Box::new(self.candidates.filter(|&local| {
+            self.ctx.current_body.local_decls[local].mutability == Mutability::Mut
+        }));
+        Self {
+            candidates,
+            ..self
+        }
     }
 
     pub fn of_ty(self, ty: Ty) -> Self {
-        self.candidates = Box::new(self.candidates.filter(|(local, decl)| decl.ty == ty));
-        self
+        let candidates = Box::new(self.candidates.filter(move |&local| self.ctx.current_body.local_decls[local].ty == ty));
+        Self {
+            candidates,
+            ..self
+        }
     }
 
-    pub fn except(self, exclude: Local) -> Self {
-        self.candidates = Box::new(self.candidates.filter(|(local, _)| *local != exclude));
-        self
-
+    pub fn except(self, exclude: &'ctx Place) -> Self {
+        // TODO: More granular place discrimination
+        let candidates = Box::new(self.candidates.filter(|&local| local != exclude.local));
+        Self {
+            candidates,
+            ..self
+        }
     }
 
-    pub fn select<R: Rng>(&self, rng: &mut R) -> Option<Place> {
+    pub fn select<R: Rng>(self, rng: &mut R) -> Option<Place> {
         self.candidates
             .choose(rng)
-            .map(|(local, _)| Place { local })
+            .map(|local| Place::from_local(local))
     }
 }
