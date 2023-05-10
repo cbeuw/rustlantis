397d73548729d3738c5eba8d4513382799f2d94e
Qian Wang <wangqian@student.ethz.ch>
2023-05-10 12:10:45 +0200

    Factor out clear env

--- difftest/src/backends.rs
+++ difftest/src/backends.rs
@@ -1,4 +1,5 @@
 use std::{
+    env,
     ffi::{OsStr, OsString},
     hash::{Hash, Hasher},
     path::{Path, PathBuf},
@@ -6,6 +7,22 @@ use std::{
 };
 
 use log::debug;
+
+trait ClearEnv {
+    fn clear_env(&mut self, preserve: &[&str]) -> &mut Command;
+}
+
+impl ClearEnv for Command {
+    fn clear_env(&mut self, preserve: &[&str]) -> &mut Command {
+        self.env_clear();
+        for env in preserve {
+            if let Ok(existing) = env::var(env) {
+                self.env(env, existing);
+            }
+        }
+        self
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct ProcessOutput {
@@ -155,8 +172,7 @@ impl Miri {
             .arg("miri")
             .arg("setup")
             .arg("--print-sysroot")
-            .env_clear()
-            .env("PATH", env!("PATH"))
+            .clear_env(&["PATH"])
             .current_dir(miri_dir)
             .output()
             .expect("can run cargo-miri setup --print-sysroot");
@@ -210,8 +226,7 @@ impl Miri {
             let output = Command::new(miri_dir.join("miri"))
                 .arg("build")
                 .arg("--release")
-                .env_clear()
-                .env("PATH", env!("PATH"))
+                .clear_env(&["PATH"])
                 .current_dir(miri_dir)
                 .output()
                 .expect("can run miri build and get output");
@@ -277,8 +292,7 @@ impl Cranelift {
             debug!("Setting up cranelift under {}", clif_dir.to_string_lossy());
             let output = Command::new(clif_dir.join("y.rs"))
                 .arg("prepare")
-                .env_clear()
-                .env("PATH", env!("PATH"))
+                .clear_env(&["PATH"])
                 .current_dir(clif_dir)
                 .output()
                 .expect("can run y.rs prepare and get output");
@@ -290,8 +304,7 @@ impl Cranelift {
 
             let output = Command::new(clif_dir.join("y.rs"))
                 .arg("build")
-                .env_clear()
-                .env("PATH", env!("PATH"))
+                .clear_env(&["PATH"])
                 .current_dir(clif_dir)
                 .output()
                 .expect("can run y.rs build and get output");
@@ -385,8 +398,7 @@ impl GCC {
 impl Backend for GCC {
     fn compile(&self, source: &Path, target: &Path) -> ProcessOutput {
         let compile_out = Command::new("rustc")
-            .env_clear()
-            .env("PATH", env!("PATH"))
+            .clear_env(&["PATH", "LD_LIBRARY_PATH"])
             .current_dir(&self.repo)
             .arg(source)
             .args([
