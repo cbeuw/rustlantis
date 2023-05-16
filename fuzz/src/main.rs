a06178b820d3d6e3825c5ba59109dac873516cf7
Qian Wang <wangqian@student.ethz.ch>
2023-05-16 12:25:19 +0200

    Remove unused code

--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -24,31 +24,6 @@ use crate::generation::GenerationCtx;
 fn main() {
     env_logger::init();
 
-    // let mut body = Body::new(&vec![Ty::Bool], Ty::Bool);
-    // let statements = vec![Statement::Assign(
-    //     Place::RETURN_SLOT,
-    //     Rvalue::UnaryOp(
-    //         UnOp::Not,
-    //         Operand::Copy(Place {
-    //             local: body.args_iter().next().unwrap(),
-    //         }),
-    //     ),
-    // )];
-    // let terminator = Some(Terminator::Return);
-    // body.new_basic_block(BasicBlockData {
-    //     statements,
-    //     terminator,
-    // });
-    // let mut program = Program::new();
-    // program.push_fn(body);
-
-    // println!("{}", program.serialize());
-
-    // let mut rng = SmallRng::seed_from_u64(0);
-    // let skeleton = build_skeleton(&mut rng);
-    // let dot = Dot::new(&skeleton);
-    // println!("{dot:?}");
-
     let seed: u64 = args().nth(1).unwrap().parse().unwrap();
     info!("Generating a program with seed {seed}");
     let genctxt = GenerationCtx::new(seed);
